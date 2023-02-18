37175718c35c67522bd67fd44128cf378754f556
Jose Daniel <99700736+Jdalucard@users.noreply.github.com>
2023-02-17 19:25:17 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Navigate, Route, Routes, BrowserRouter } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import ProfilePage from "scenes/profilePage";
+import LoginPage from "scenes/loginPage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { boolean } from "yup";
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = boolean(useSelector((state) => state.token));
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;

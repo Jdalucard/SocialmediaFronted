17a0ce1e55cca4d79f93778e7b93716eac736bdc
Jose Daniel <99700736+Jdalucard@users.noreply.github.com>
2023-02-18 11:25:40 -0500

    "netlfly"

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -10,6 +10,7 @@ const UserImage = ({ image, size = "60px" }) => {
         alt="user"
         src={`https://socialmedia-2vgd.onrender.com/assets/${image}`}
       />
+      {/* actualizado rutas  */}
     </Box>
   );
 };
